54c64672b9c2c6db97c18f7c9d86a6400f594f7b
XTJP1 <56214206+XTJP1@users.noreply.github.com>
2020-06-30 11:43:42 +0900

    Japanese translation

---
+++ src/TopSites/TopSites.ja.ts
@@ -0,0 +1,74 @@
+/// <reference types="lipsurf-types/extension"/>
+import TopSitesPlugin from "./TopSites";
+
+TopSitesPlugin.languages!.ja = {
+    niceName: 'トップサイト',
+    description: '人気のウェブサイトへのショートカットです。',
+    homophones: {
+
+    },
+    authors: "Miko, Hanna, Hiroki",
+
+    commands: {
+        "Youtube": {
+            name: 'Youtube',
+            match: ['ゆーちゅーぶ', 'youtube']
+        },
+
+        "Wikipedia": {
+            name: 'Wikipedia',
+            match: ['うぃきぺでぃあ', 'wikipedia']
+            },
+
+        "Facebook": {
+            name: 'Facebook',
+            match: ['ふぇいすぶっく', 'ふぇーすぶっく', 'facebook']
+        },
+
+        "Twitter": {
+            name: 'Twitter',
+            match: ['ついったー', 'twitter']
+        },
+
+        "Instagram": {
+            name: 'Instagram',
+            match: ['いんすたぐらむ', 'いんすた', 'instagram']
+        },
+        "Amazon": {
+            name: 'Amazon',
+            match: ['あまぞん', 'amazon']
+        },
+        "Ebay": {
+            name: 'Ebay',
+            match: ['いーべい', 'いーべー', 'ebay']
+        },
+        "VK": {
+            name: 'VK',
+            match: ['ぶいけー', "vk"]
+        },
+        "Netflix": {
+            name: 'Netflix',
+            match: ['ねっとふりっくす', 'ねふり', 'netflix']
+        },
+        "Twitch": {
+            name: 'Twitch',
+            match: ['ついっち', 'twitch']
+        },
+        "New York Times": {
+            name: 'New York Times',
+            match: ['にゅーよーくたいむず', 'にゅーよーくたいむす', 'new york times']
+        },
+        "Github": {
+            name: 'Github',
+            match: ['ぎっとはぶ', 'github']
+        },
+        "Hacker News": {
+            name: 'Hacker News',
+            match: ['はっかーにゅーす', 'hacker news'],
+        },
+        "Product Hunt": {
+            name: 'Product Hunt',
+            match: ['ぷろだくとはんと', 'product hunt']
+        },
+        }
+};
